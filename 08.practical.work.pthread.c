350edcd4e07c3f2e8f56823f7fa1606cb064afa7
provodoihi <profiloes@gmail.com>
2020-04-03 10:09:56 +0700

    Add some command and add fibonacci part

--- 08.practical.work.pthread.c
+++ 08.practical.work.pthread.c
@@ -3,8 +3,9 @@
 #include <pthread.h>
 void *prime_num(void *param)
 {
-   int i,j;
-    for (i=2 ;i<=1000000 ; i++)
+    int i,j;
+    printf("Prime number from 2 to 1000000:\n");
+    for (i=2 ;i<=100 ; i++)
     {
         for (j=2; j<=i; j++)
         {
@@ -17,13 +18,30 @@ void *prime_num(void *param)
         {
             printf("%d \n",i);
         }
-    } 
+    }
 }
-int main() 
+
+void *fibo_seq(void *param)
 {
-    pthread_t pid;
-    pthread_create(&pid, NULL, prime_num, NULL); 
+    int num1=2, num2=3, numnext;
+    printf("Here is Fibonacci Sequence to 1000000: %d, %d, ",num1,num2);
+    numnext=num1+num2;
+    while (numnext<=100)
+    {
+        printf("%d, ",numnext);
+        num1=num2;
+        num2=numnext;
+        numnext=num1+num2;
+    }
+}
+
+int main()
+{
+    pthread_t pid,pid1;
+    pthread_create(&pid, NULL, prime_num, NULL);
+    pthread_create(&pid1, NULL, fibo_seq, NULL);
     pthread_join(pid,NULL);
+    pthread_join(pid1,NULL);
     system("PAUSE");
     return 0;
 }
